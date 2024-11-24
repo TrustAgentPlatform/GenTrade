f3a12f29c49207eb2a157f2096d23b3983d2eb58
Lu Ken <bluewish.ken.lu@live.cn>
2024-11-24 20:01:22 +0800

    add initial structure
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- src/tia/market_data/core.py
+++ src/tia/market_data/core.py
@@ -1,57 +1,216 @@
 """
 DataHub Core Package
 """
+import logging
+import pandas as pd
+import time
 from abc import ABC, abstractmethod
 
-class Instrument(ABC):
+LOG = logging.getLogger(__name__)
+
+# Forward Declaration
+class FinancialMarket(ABC):
+    pass
+
+class FinancialAsset(ABC):
     """
     Trading instruments are all the different types of assets and contracts that
     can be traded. Trading instruments are classified into various categories,
     some more popular than others.
     """
 
-    def __init__(self, name:str):
-        self._name = name
+    # The detle of TIME FRAME in seconds
+    TIME_FRAME = {
+        '15m':          15 * 60,
+        '1h':       1 * 60 * 60,
+        '4h':       4 * 60 * 60,
+        '1d':      24 * 60 * 60,
+        '1W':  7 * 24 * 60 * 60,
+        '1M': 30 * 24 * 60 * 60
+    }
+
+    def __init__(self, name:str, market:FinancialMarket):
+        self._name:str = name
+        self._market:FinancialMarket = market
+        self._cache:dict[str, pd.DataFrame] = {}
 
     @property
     def name(self) -> str:
-        self._name
-
-
-class MarketProvider(ABC):
-
-    """
-    It could be binance, gateio or others who provide market data.
-    """
-
-    def __init__(self, name:str):
-        self._name = name
-
-    @property
-    def name(self) -> str:
+        """
+        Property name
+        """
         return self._name
 
+    @property
+    def market(self) -> FinancialMarket:
+        """
+        Property market which belong to
+        """
+        return self._market
 
-class Market(ABC):
+    def fetch_ohlcv(self, timeframe:str="1h", since: int = -1,
+                    limit: int = 100) -> pd.DataFrame:
+        """
+        Fetch the specific asset
+        """
+        # correct limit to ensure correct range according to since
+        if since != -1:
+            max_limit = int((time.time() - since) / self.TIME_FRAME[timeframe])
+            if limit > max_limit:
+                limit = max_limit
+
+        # try to get data from cache firstly
+        df_cached, since_new, limit_new = \
+            self._fetch_ohlcv_from_cache(timeframe, since, limit)
+        if df_cached is None:
+            LOG.info("Fetch ohlcv from market: timeframe:%s since:%s, limit:%d",
+                     timeframe, since, limit)
+            df = self._market.fetch_ohlcv(self, timeframe, since, limit)
+            self._save_ohlcv_to_cache(timeframe, df)
+            return df
+        else:
+            if limit_new == 0:
+                # already find all data from cache, return directly
+                return df_cached
+            else:
+                # find part of data from cache, need continue to get others
+                df_part = self._market.fetch_ohlcv(
+                    self, timeframe, since_new, limit_new)
+                self._save_ohlcv_to_cache(timeframe, df_part)
+                return self._merge_df(df_cached, df_part)
+
+    def _merge_olhcv(self, df1:pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+        """
+        Merge two OLHCV data frame
+        """
+        df_merged = pd.concat([df1, df2]).drop_duplicates()
+        df_merged.sort_index(inplace=True)
+        return df_merged
+
+    def _fetch_ohlcv_from_cache(self, timeframe, since, limit) -> pd.DataFrame:
+        """
+        Fetch OHLCV from cache
+        """
+        LOG.info("featch_ohlcv_from_cache: since=%d, limit=%d now=%d" % \
+                 (since, limit, time.time()))
+        if timeframe not in self.TIME_FRAME:
+            LOG.error("Time frame %s is invalid" % timeframe)
+            return None, since, limit
+
+        # if the cache for specific time frame has not been created, then create
+        if timeframe not in self._cache:
+            self._cache[timeframe] = pd.DataFrame()
+            return None, since, limit
+
+        tf_delta = self.TIME_FRAME[timeframe]
+        # calculate the range from_ -> to_
+        if since == -1:
+            to_ = int(time.time() / tf_delta - 1) * tf_delta
+            from_ = to_ - (limit - 1) * tf_delta
+        else:
+            from_ = int(since / tf_delta) * tf_delta
+            to_ = since + (limit - 1) * tf_delta
+
+        LOG.info("from=%d->to=%d first=%d->last=%d" % (
+            from_, to_, self._cache[timeframe].index[0],
+            self._cache[timeframe].index[-1]))
+
+        # if from_ is too small or too big, then fetch from remote directly.
+        if from_ < self._cache[timeframe].index[0] or \
+            from_ > self._cache[timeframe].index[-1]:
+            LOG.info("Not found records from cache")
+            return None, since, limit
+
+        # if from_ in the range of existing cache
+        if to_ <= self._cache[timeframe].index[-1]:
+            LOG.info("Found all records from cache")
+            df_part = self._cache[timeframe].loc[from_:to_]
+            if self._check_whether_continuous(df_part, timeframe):
+                return df_part, since, 0
+
+        else:
+            LOG.info("Found part of records from cache")
+            df_part = self._cache[timeframe].loc[from_:]
+            if self._check_whether_continuous(df_part, timeframe):
+                new_limit = limit - \
+                    int((to_ - self._cache[timeframe].index[-1]) / tf_delta)
+                new_since = self._cache[timeframe].index[-1]
+                return df_part, new_since, new_limit
+
+        return None, since, limit
+
+    def _check_whether_continuous(self, df:pd.DataFrame, timeframe):
+        """
+        Check whether the dataframe is continuous
+        """
+        count = int((df.index[-1] - df.index[0]) / self.TIME_FRAME[timeframe]) \
+            + 1
+        if count != len(df):
+            LOG.error("The data frame is not continuous: count=%d, len=%d" %
+                      (count, len(df)))
+            return False
+        return True
+
+    def _save_ohlcv_to_cache(self, timeframe:str, df_new:pd.DataFrame):
+        """
+        Save OHLCV to cache
+        """
+        self._cache[timeframe] = self._merge_olhcv(
+            self._cache[timeframe], df_new)
+        return self._cache[timeframe]
+
+    def ohlvc_to_datetime(self, df:pd.DataFrame):
+        df.index = pd.to_datetime(df.index, unit="ms")
+        return df
+
+class FinancialMarket(ABC):
+
+    MARKET_CRYPTO = 'crypto'
+    MARKET_STOCK = 'stock'
 
     """
     Trading market includes crypto, stock or golden.
     """
 
-    def __init__(self, name:str, market_provider:MarketProvider):
+    def __init__(self, name:str, market_type:str):
+        assert market_type in \
+            [FinancialMarket.MARKET_CRYPTO, FinancialMarket.MARKET_STOCK]
         self._name = name
-        self._provider = market_provider
-        self._instruments:dict[str, Instrument] = {}
+        self._assets:dict[str, FinancialAsset] = {}
 
     @property
     def name(self) -> str:
+        """
+        Property: name
+        """
         return self._name
 
     @property
-    def provider(self) -> MarketProvider:
-        return self._provider
+    def assets(self) -> dict[str, FinancialAsset]:
+        """
+        Property: assets
+        """
+        return self._assets
 
-    def get_instrument(self, it_name) -> Instrument:
-        if it_name in self._instruments:
-            return self._instruments[it_name]
+    def get_asset(self, name) -> FinancialAsset:
+        """
+        Get instrument object from its name
+        """
+        if name in self._assets:
+            return self._assets[name]
         return None
+
+    @abstractmethod
+    def init(self):
+        """
+        Financial Market initialization
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def fetch_ohlcv(self, asset:FinancialAsset, timeframe:str, since: int = None,
+                    limit: int = None):
+        """
+        Fetch OHLV for the specific asset
+        """
+        raise NotImplementedError
--- src/tia/market_data/crypto.py
+++ src/tia/market_data/crypto.py
@@ -1,13 +1,120 @@
+import os
+import logging
+import ccxt
+import time
+import pandas as pd
+import numpy as np
+from core import FinancialAsset, FinancialMarket
 
-from .core import Instrument, Market, MarketProvider
+LOG = logging.getLogger(__name__)
 
+class CryptoMarket(FinancialMarket):
 
-class CryptoMarket(Market):
+    def __init__(self, name: str):
+        super().__init__(name, FinancialMarket.MARKET_CRYPTO)
+
+    def get_crypto_asset(self, base, quote):
+        """
+        Return asset according to crypt naming rule "base/quote"
+        """
+        return self.get_asset("%s/%s" % (base.lower(), quote.lower()))
+
+class CryptoAsset(FinancialAsset):
+
+    def __init__(self, currency_base:str, currency_quote:str,
+                 symbol:str, market:CryptoMarket):
+        self._currency_base = currency_base.lower()
+        self._currency_quote = currency_quote.lower()
+        self._symbol = symbol
+        super().__init__("%s/%s" %(currency_base.lower(),
+                                   currency_quote.lower()), market)
+
+    @property
+    def currency_base(self) -> str:
+        return self._currency_base
+
+    @property
+    def currency_quote(self) -> str:
+        return self.currency_quote
+
+    @property
+    def symbol(self) -> str:
+        return self._symbol
+
+class BinanceMarket(CryptoMarket):
 
     def __init__(self):
-        Market.__init__(self, "crypto")
+        super().__init__("Binance")
+        self._ccxt_inst = ccxt.binance({'apiKey': self.api_key,
+                                        'secret': self.api_secret})
+
+    @property
+    def api_key(self):
+        return os.getenv("TIA_BINANCE_API_KEY")
+
+    @property
+    def api_secret(self):
+        return os.getenv("TIA_BINANCE_API_SECRET")
+
+    def init(self):
+        LOG.info("Loading Binance Market...")
+        retry_num = 0
+        while retry_num < 5:
+            try:
+                self._ccxt_inst.load_markets()
+                break
+            except:
+                retry_num += 1
+                LOG.error("Fail to load market... retry")
+                time.sleep(1)
+
+        for symbol in self._ccxt_inst.symbols:
+            base, quote = symbol.split("/")
+            caobj = CryptoAsset(base, quote, symbol, self)
+            self.assets[caobj.name] = caobj
+        LOG.info("Found %d crypto assets." % len(self.assets))
+
+    def fetch_ohlcv(self, asset:CryptoAsset, timeframe: str, since: int = -1,
+                    limit: int = 500):
+        LOG.info("Fetch from market: timeframe=%s since=%d, limit=%d" % \
+                  (timeframe, since, limit))
+        remaining = limit
+        delta = FinancialAsset.TIME_FRAME[timeframe]
+        all_ohlcv = []
+
+        if since == -1:
+            to_ = int(time.time() / delta - 1) * delta
+            since = to_ - (limit - 1) * delta
+        else:
+            max_limit = int((time.time() - since) / delta)
+            if limit > max_limit:
+                limit = max_limit
+            to_ = int((since + (limit - 1)* delta) / delta) * delta
+
+        # Continuous to fetching until get all data
+        while remaining > 0:
+            ohlcv = self._ccxt_inst.fetch_ohlcv(asset.symbol, timeframe,
+                                                since * 1000, limit)
+            all_ohlcv += ohlcv
+            remaining = remaining - len(ohlcv)
+            since = to_ - remaining * delta
+            time.sleep(0.1)
+
+        df = pd.DataFrame(all_ohlcv, columns =
+                          ['time', 'open', 'high', 'low', 'close', 'vol'])
+        df.time = (df.time / 1000).astype(np.int64)
+        df.set_index('time', inplace=True)
+        return df
 
 
-class CryptoAsses(Instrument):
-
-    pass
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    cm = BinanceMarket()
+    cm.init()
+    asset_btcusdt = cm.get_crypto_asset("ETH", "USDT")
+    ohlcv = asset_btcusdt.fetch_ohlcv("1h", since=1732514400, limit=1)
+    print(ohlcv)
+    ohlcv = asset_btcusdt.fetch_ohlcv("1h", since=1732532400, limit=1)
+    print(ohlcv)
+    ohlcv = asset_btcusdt.fetch_ohlcv("1h", since=1732536000)
+    print(ohlcv)
