e3f25fa26a91f7c4fe39823e0dcfe5a18e1cd291
Lu Ken <bluewish.ken.lu@live.cn>
2024-11-28 19:25:49 +0800

    action: add python check (#2)
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def start_trading(crypto_name:str):
-    st.header(crypto_name)
-    tab1, tab2 = st.tabs(['Market', 'BackTest'])
-
-crypto_name = st.sidebar.selectbox("Choose a crypto", ["BTCUSDT", "ETHUSDT"])
-start_trading(crypto_name)
