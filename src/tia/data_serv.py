795b284eff3afe3ef1876be83e44a0e1f755355c
Lu Ken <bluewish.ken.lu@live.cn>
2024-11-29 00:01:19 +0800

    add fetch ohlcv
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- src/tia/data_serv.py
+++ src/tia/data_serv.py
@@ -29,12 +29,15 @@ class DataServer:
 
     please set `TIA_CACHE_DIR`.
     """
+
+    _inst = None
+
     def __init__(self) -> None:
         self._cache_dir = os.getenv("TIA_CACHE_DIR", None)
         LOG.info("Cache directory is %s", self._cache_dir)
         crypto_market = BinanceMarket(cache_dir=self._cache_dir)
         self._markets:dict[str, FinancialMarket] = {
-            crypto_market.name: BinanceMarket(cache_dir=self._cache_dir)
+            crypto_market.market_id: BinanceMarket(cache_dir=self._cache_dir)
         }
         for market in self._markets.values():
             if not market.init():
@@ -44,15 +47,33 @@ class DataServer:
     def markets(self):
         return self._markets
 
-dataserv = DataServer()
+    @staticmethod
+    def inst():
+        if DataServer._inst is None:
+            DataServer._inst = DataServer()
+        return DataServer._inst
+
+data_server = DataServer.inst()
 
 @app.get("/get_markets")
 async def get_markets():
     retval = {}
-    for key, market in dataserv.markets.items():
+    for key, market in data_server.markets.items():
         retval[key] = {
             "name": market.name,
             "type": market.market_type,
-            "id": market.market_id
         }
     return retval
+
+@app.get("/get_ohlcv")
+async def get_ohlcv(market_id:str, asset:str="BTC_USDT",
+                    timeframe:str="1m", since:int=-1, limit:int=10):
+    if market_id not in data_server.markets:
+        return None
+    market_obj = data_server.markets[market_id]
+
+    for key, _ in market_obj.assets.items():
+        print(key)
+    asset_obj = market_obj.get_asset(asset.lower())
+    ret = asset_obj.fetch_ohlcv(timeframe, since, limit)
+    return ret.to_json(orient="records")
