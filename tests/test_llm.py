3b9084655d790a790f11e8b8f69f3014e4fb8163
Lu Ken <bluewish.ken.lu@live.cn>
2025-09-14 12:39:30 +0800

    extract the LLM's fixture to standalone module
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -15,14 +15,11 @@ Test LLM compatible call on ChatOpenAI and tools usage.
 
 
 """
-import os
-import time
+
 from typing import Optional
 
-import pytest
 from pydantic import BaseModel, Field
 
-from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_core.messages import ToolMessage
 
@@ -30,86 +27,7 @@ from langchain_tavily import TavilySearch
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
 
-class CustomChatOpenAI(ChatOpenAI):
-
-    def invoke(self, messages, config=None, **kwargs):
-        start_time = time.perf_counter()
-
-        response = super().invoke(messages, config=config, **kwargs)
-
-        duration = time.perf_counter() - start_time
-        token_usage = response.response_metadata["token_usage"]
-        print(f"[Token] Prompt: {token_usage.get('prompt_tokens')}, "\
-              f"Completion: {token_usage.get('completion_tokens')}, "\
-              f"Total: {token_usage.get('total_tokens')} "\
-              f"[Duration] {duration:.2f} seconds")
-
-        return response
-
-@pytest.fixture(params=[
-    {
-        "type": "qwen-turbo",
-        "provider": "dashscope",
-        "model": "qwen-turbo",
-        "temperature": 0.1
-    },
-    {
-        "type": "deepseek-r1",
-        "provider": "dashscope",
-        "model": "deepseek-r1",
-        "temperature": 0.1
-    },
-    {
-        "type": "claude-3.5-sonnet",
-        "provider": "openrouter",
-        "model": "anthropic/claude-3.5-sonnet",
-        "temperature": 0.1
-    },
-    {
-        "type": "llama-4-scout",
-        "provider": "openrouter",
-        "model": "meta-llama/llama-4-scout:free",
-        "temperature": 0.1
-    },
-    {
-        "type": "gemini-2.0-flash",
-        "provider": "openrouter",
-        "model": "google/gemini-2.0-flash-001",
-        "temperature": 0.1
-    },
-    {
-        "type": "glm-4-9b-chat",
-        "provider": "siliconflow",
-        "model": "THUDM/glm-4-9b-chat",
-        "temperature": 0.1
-    },
-], ids=["qwen-turbo", "deepseek-r1", "claude-3.5-sonnet", \
-        "llama-4-scout", "gemini-2.0-flash", "glm-4-9b-chat"])
-def llm_instance(request):
-    config = request.param  # 获取当前参数
-
-    if config["provider"] == "dashscope":
-        api_key = os.getenv("DASHSCOPE_API_KEY")
-        base_url = os.getenv("DASHSCOPE_BASE_URL",
-            "https://dashscope.aliyuncs.com/compatible-mode/v1")
-    elif config["provider"] == "openrouter":
-        api_key = os.getenv("OPENROUTER_API_KEY")
-        base_url = os.getenv("OPENROUTER_BASE_URL", "https://openrouter.ai/api/v1")
-    elif config["provider"] == "siliconflow":
-        api_key = os.getenv("SILICONFLOW_API_KEY")
-        base_url = os.getenv("SILICONFLOW_BASE_URL", "https://api.siliconflow.cn/v1")
-    else:
-        assert False, "Unsupported provider"
-
-    assert api_key is not None, "API key is not set"
-
-    return CustomChatOpenAI(
-        model=config["model"],
-        temperature=config["temperature"],
-        max_tokens=200,
-        api_key=api_key,
-        base_url=base_url
-    )
+from fixture_llm import llm_instance                # pylint: disable=unused-import
 
 def test_tool_basic(llm_instance):
     llminst = llm_instance
