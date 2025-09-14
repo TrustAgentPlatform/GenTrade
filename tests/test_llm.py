ff253275e4772c833c4eec55f813b49a8bf7ccaf
Lu Ken <bluewish.ken.lu@live.cn>
2025-09-14 21:26:32 +0800

    add test case for worfklow provied by langgraph
    Signed-off-by: Lu Ken <bluewish.ken.lu@live.cn>

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -17,6 +17,7 @@ Test LLM compatible call on ChatOpenAI and tools usage.
 """
 
 from typing import Optional
+from typing_extensions import TypedDict
 
 from pydantic import BaseModel, Field
 
@@ -26,6 +27,7 @@ from langchain_core.messages import ToolMessage
 from langchain_tavily import TavilySearch
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.prebuilt import create_react_agent
+from langgraph.graph import StateGraph, START, END
 
 from fixture_llm import llm_instance                # pylint: disable=unused-import
 
@@ -165,3 +167,79 @@ def test_tool_structured_output(llm_instance):
     print("Joke Punchline:", response.punchline)
     if response.rating is not None:
         print("Joke Rating:", response.rating)
+
+# Graph state
+class State(TypedDict):
+    topic: str
+    joke: str
+    improved_joke: str
+    final_joke: str
+
+def test_llm_graph(llm_instance):
+
+    # Nodes
+    def generate_joke(state: State):
+        """First LLM call to generate initial joke"""
+
+        msg = llm_instance.invoke(f"Write a short joke about {state['topic']}")
+        return {"joke": msg.content}
+
+
+    def check_punchline(state: State):
+        """Gate function to check if the joke has a punchline"""
+
+        # Simple check - does the joke contain "?" or "!"
+        if "?" in state["joke"] or "!" in state["joke"]:
+            return "Pass"
+        return "Fail"
+
+
+    def improve_joke(state: State):
+        """Second LLM call to improve the joke"""
+
+        msg = llm_instance.invoke(f"Make this joke funnier by adding wordplay: {state['joke']}")
+        return {"improved_joke": msg.content}
+
+
+    def polish_joke(state: State):
+        """Third LLM call for final polish"""
+
+        msg = llm_instance.invoke(f"Add a surprising twist to this joke: {state['improved_joke']}")
+        return {"final_joke": msg.content}
+
+    # Build workflow
+    workflow = StateGraph(State)
+
+    # Add nodes
+    workflow.add_node("generate_joke", generate_joke)
+    workflow.add_node("improve_joke", improve_joke)
+    workflow.add_node("polish_joke", polish_joke)
+
+    # Add edges to connect nodes
+    workflow.add_edge(START, "generate_joke")
+    workflow.add_conditional_edges(
+        "generate_joke", check_punchline, {"Fail": "improve_joke", "Pass": END}
+    )
+    workflow.add_edge("improve_joke", "polish_joke")
+    workflow.add_edge("polish_joke", END)
+
+    # Compile
+    chain = workflow.compile()
+
+    # Show workflow
+    #display(Image(chain.get_graph().draw_mermaid_png()))
+
+    # Invoke
+    state = chain.invoke({"topic": "cats"})
+    print("Initial joke:")
+    print(state["joke"])
+    print("\n--- --- ---\n")
+    if "improved_joke" in state:
+        print("Improved joke:")
+        print(state["improved_joke"])
+        print("\n--- --- ---\n")
+
+        print("Final joke:")
+        print(state["final_joke"])
+    else:
+        print("Joke failed quality gate - no punchline detected!")
